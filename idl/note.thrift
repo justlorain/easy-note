003d2fde3fe7ba4ea93879c0fd799d37c81415e0
Lorain <743804605@qq.com>
2022-11-22 19:37:55 +0800

    add idl

---
+++ idl/note.thrift
@@ -0,0 +1,77 @@
+namespace go notedemo
+
+struct BaseResp {
+    1: i64 status_code
+    2: string status_message
+    3: i64 service_time
+}
+
+struct Note {
+    1: i64 note_id
+    2: i64 user_id
+    3: string username
+    4: string user_avatar
+    5: string title
+    6: string content
+    7: i64 create_time
+}
+
+struct CreateNoteRequest {
+    1: string title (api.form="title")
+    2: string content (api.form="content")
+    3: i64 user_id
+}
+
+struct CreateNoteResponse {
+    1: BaseResp base_resp
+}
+
+struct DeleteNoteRequest {
+    1: i64 note_id (api.path="note_id")
+    2: i64 user_id
+}
+
+struct DeleteNoteResponse {
+    1: BaseResp base_resp
+}
+
+struct UpdateNoteRequest {
+    1: i64 note_id (api.path="note_id")
+    2: i64 user_id
+    3: optional string title (api.form="title")
+    4: optional string content (api.form="content")
+}
+
+struct UpdateNoteResponse {
+    1: BaseResp base_resp
+}
+
+struct QueryNoteRequest {
+    1: i64 user_id
+    2: optional string search_key (api.query="search_key")
+    3: i64 offset (api.query="offset")
+    4: i64 limit (api.query="limit")
+}
+
+struct QueryNoteResponse {
+    1: list<Note> notes
+    2: i64 total
+    3: BaseResp base_resp
+}
+
+struct MGetNoteRequest {
+    1: list<i64> note_ids
+}
+
+struct MGetNoteResponse {
+    1: list<Note> notes
+    2: BaseResp base_resp
+}
+
+service NoteService {
+    CreateNoteResponse CreateNote(1: CreateNoteRequest req) (api.post="/v2/note")
+    DeleteNoteResponse DeleteNote(1: DeleteNoteRequest req) (api.delete="/v2/note/:note_id")
+    UpdateNoteResponse UpdateNote(1: UpdateNoteRequest req) (api.put="/v2/note/:note_id")
+    QueryNoteResponse QueryNote(1: QueryNoteRequest req) (api.get="/v2/note/query")
+    MGetNoteResponse MGetNote(1: MGetNoteRequest req)
+}

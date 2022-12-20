f2eadbdaa6a83d5a5d8e9013723395b8c328a6db
Anmory <binaryanmory@gmail.com>
2022-12-21 01:20:49 +0800

    add kitex gen with validator plugin

---
+++ temp/note/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	demonote "easy-note/temp/note/kitex_gen/demonote/noteservice"
+	"log"
+)
+
+func main() {
+	svr := demonote.NewServer(new(NoteServiceImpl))
+
+	err := svr.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
---
+++ temp/user/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	demouser "easy-note/temp/user/kitex_gen/demouser/userservice"
+	"log"
+)
+
+func main() {
+	svr := demouser.NewServer(new(UserServiceImpl))
+
+	err := svr.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
